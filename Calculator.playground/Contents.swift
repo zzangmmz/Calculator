c5e60a2aa5f0a4d7f886b0e09382f042d200ac40
zzangmmz <leemj036@gmail.com>
2024-10-30 21:02:40 +0900

    feat: AbstractOperation 프로토콜 구현

--- Calculator.playground/Contents.swift
+++ Calculator.playground/Contents.swift
@@ -1,2 +1,8 @@
 import UIKit
 
+protocol AbstractOperation {
+    func addOperation(_ a: Double, _ b: Double) -> Double
+    func subSubtractOperation(_ a: Double, _ b: Double) -> Double
+    func multiplyOperation(_ a: Double, _ b: Double) -> Double
+    func divideOperation(_ a: Double, _ b: Double) throws -> Double
+}
