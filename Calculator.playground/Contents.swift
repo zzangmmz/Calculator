fbef520a0b7be4b79f84d63da26fe4778a2f4c98
zzangmmz <leemj036@gmail.com>
2024-10-30 21:03:53 +0900

    feat: 프로토콜 채택한 Calculator 클래스 구현

--- Calculator.playground/Contents.swift
+++ Calculator.playground/Contents.swift
@@ -4,5 +4,23 @@ protocol AbstractOperation {
     func addOperation(_ a: Double, _ b: Double) -> Double
     func subSubtractOperation(_ a: Double, _ b: Double) -> Double
     func multiplyOperation(_ a: Double, _ b: Double) -> Double
-    func divideOperation(_ a: Double, _ b: Double) throws -> Double
+    func divideOperation(_ a: Double, _ b: Double) -> Double
+}
+
+class Calculator: AbstractOperation {
+    func addOperation(_ a: Double, _ b: Double) -> Double {
+        return a + b
+    }
+    
+    func subSubtractOperation(_ a: Double, _ b: Double) -> Double {
+        return a - b
+    }
+    
+    func multiplyOperation(_ a: Double, _ b: Double) -> Double {
+        return a * b
+    }
+    
+    func divideOperation(_ a: Double, _ b: Double) -> Double {
+        return a / b
+    }
 }
